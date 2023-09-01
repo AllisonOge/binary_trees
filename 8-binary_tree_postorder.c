17344ac22b579b3c615ed1a0822f761485f70304
Allison Ogechukwu <44110875+AllisonOge@users.noreply.github.com>
2023-09-01 04:46:28 +0000

    task 8 betty formatting

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -2,7 +2,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - goes through a binary tree using post-order traversal
+ * binary_tree_postorder - traverse the tree using post-order traversal
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to callback function for each node
  * Return: nothing
