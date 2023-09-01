594e981ba013d40f9bcf22f13eb0fda189252317
Allison Ogechukwu <44110875+AllisonOge@users.noreply.github.com>
2023-09-01 05:11:56 +0000

    task 12 count the number of leaves in a tree

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - Counts the leaves in a binary tree
+ * @tree: Pointer to the root node of the tree to count the number of leaves
+ *
+ * Return: Number of leaves in a binary tree
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leaves = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		leaves++;
+
+	if (tree->left != NULL)
+		leaves += binary_tree_leaves(tree->left);
+
+	if (tree->right != NULL)
+		leaves += binary_tree_leaves(tree->right);
+
+	return (leaves);
+}
